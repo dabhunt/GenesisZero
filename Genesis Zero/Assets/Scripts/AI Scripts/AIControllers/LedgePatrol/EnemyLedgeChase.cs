83dd28bf9a4b6dd7c6daba13b302441bc319f754
CGEyeGuy <cgeyeguy@gmail.com>
2020-01-23 22:45:47 -0800

    BaseEnemyWorkflow

---
+++ Genesis Zero/Assets/Scripts/AI Scripts/AIControllers/LedgePatrol/EnemyLedgeChase.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLedgeChase : StateMachineBehaviour
+{
+    Vector3 startpos;
+    Enemy owner;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       
+        owner = animator.GetComponent<Enemy>();
+        startpos = (Vector3)owner.transform.position;
+        Debug.Log(owner.name + "Entering Chase State");
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (owner.distancefromTarget() > owner.targetdistance)
+        {
+            animator.SetTrigger("Patrol");
+          //  animator.SetTrigger("Chase");
+        }
+        
+        var target = (Vector3)owner.GetTarget().position;
+        var currentpos = (Vector3)owner.transform.position;
+
+        Debug.DrawLine(currentpos, target, Color.red);
+        // Vector2 targetposition= Vector2.MoveTowards(currentpos, target, 1 * Time.deltaTime);
+        Vector3 dirFromAtoB = (target - currentpos).normalized;
+
+        Debug.Log(owner.name+" speed="+owner.GetSpeed().GetValue());
+
+        float isfacing = Vector3.Dot(dirFromAtoB, owner.transform.right);
+
+        Vector3 targetposition = currentpos + ((dirFromAtoB * .1f));// *Time.deltaTime);
+        targetposition.y = startpos.y;
+        Debug.Log(owner.name + ":is facing val= " + isfacing);
+        if (isfacing < 0)
+        {
+            owner.transform.Rotate(0, 180, 0);
+        }
+
+        targetposition.z = currentpos.z;
+        owner.transform.position = targetposition;
+
+       
+    }
+
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log(owner.name + "Exiting Chase State");
+        animator.ResetTrigger("Chase");
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ Genesis Zero/Assets/Scripts/AI Scripts/Enemy.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Pawn
+{
+    private float radius = 1f;
+    Transform target;
+    Animator ani;
+    public Transform raypointtarget;
+    public float targetdistance = 2f;
+    public float checkerdistance = .1f;
+    
+    public void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        base.Start();
+    }
+
+    public void Update()
+    {
+        base.Update();
+    }
+
+    public Transform GetTarget()
+    {
+        return target;
+    }
+
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
+    }
+
+    public bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(raypointtarget.position, -Vector2.up, checkerdistance);
+        if (hit.collider != null)
+        {
+
+            return true;
+        }
+        else { return false; }
+    }
+
+    public bool inRange(float distance)
+    {
+        // Debug.Log(gameObject.name + ": distance from player is:" + Vector3.Distance(this.transform.position, target.position));
+        if (Vector3.Distance(transform.position, GetTarget().position) < distance)
+            return true;
+        return false;
+    }
+
+    public float distancefromTarget()
+    {
+        return Vector3.Distance(transform.position, GetTarget().position);
+    }
+
+    //getter and setter for radius
+    public float Radius { get => radius; set => radius = value; }
+    public Animator Ani { get => ani; set => ani = value; }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, targetdistance);
+    }
+}
