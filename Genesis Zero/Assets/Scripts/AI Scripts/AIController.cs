7e31be7a5e5102578b76e8bbe23686e26480f6eb
Justin Couch <couch.justin.95@gmail.com>
2020-01-26 17:14:48 -0800

    Added AIState enum and set AIController to derive from Pawn class

--- Genesis Zero/Assets/Scripts/AI Scripts/AIController.cs
+++ Genesis Zero/Assets/Scripts/AI Scripts/AIController.cs
@@ -6,7 +6,9 @@ using UnityEngine;
  * Justin Couch
  * AIControl is the base class controlling the behavior states of enemies and how they interact with the player.
  */
-public class AIController : MonoBehaviour
+public class AIController : Pawn
 {
     public AIPropertyObject BehaviorProperties;
+
+    public enum AIState { Patrolling, Following, Attacking, CoolingDown }
 }
