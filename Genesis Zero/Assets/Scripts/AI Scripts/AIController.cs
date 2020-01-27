5f09ba21d3cfb289a2abf499e449b3026254b916
Justin Couch <couch.justin.95@gmail.com>
2020-01-26 16:54:20 -0800

    Created AIController and AIPropertyObject scripts

---
+++ Genesis Zero/Assets/Scripts/AI Scripts/AIController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Justin Couch
+ * AIControl is the base class controlling the behavior states of enemies and how they interact with the player.
+ */
+public class AIController : MonoBehaviour
+{
+
+}
---
+++ Genesis Zero/Assets/Scripts/AI Scripts/AIPropertyObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AI/Create Behavior Property")]
+/**
+ * Justin Couch
+ * ScriptableObject acting as a serializable container for AI behavior properties.
+ * Attach to AIController script on an enemy.
+ */
+public class AIPropertyObject : ScriptableObject
+{
+    public float DetectRadius;
+    public float AvoidRadius;
+    public float AttackChargeTime;
+    public float AttackCooldownTime;
+}
