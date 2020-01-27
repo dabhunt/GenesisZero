210a76b70eefe0624f50c8ff2f42ab0c5ed17d9b
Justin Couch <couch.justin.95@gmail.com>
2020-01-26 21:28:32 -0800

    Added BasicMove script for testing

---
+++ Genesis Zero/Assets/Scripts/AI Scripts/BasicMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Justin Couch
+ * Basic movement script for testing.
+ */
+public class BasicMove : MonoBehaviour
+{
+    public float MoveSpeed = 1.0f;
+
+    void Update()
+    {
+        Vector3 moveDir = Vector3.zero;
+        moveDir += Input.GetKey(KeyCode.UpArrow) ? Vector3.up : Vector3.zero;
+        moveDir += Input.GetKey(KeyCode.DownArrow) ? Vector3.down : Vector3.zero;
+        moveDir += Input.GetKey(KeyCode.RightArrow) ? Vector3.right : Vector3.zero;
+        moveDir += Input.GetKey(KeyCode.LeftArrow) ? Vector3.left : Vector3.zero;
+        transform.Translate(moveDir.normalized * MoveSpeed * Time.deltaTime);
+    }
+}
