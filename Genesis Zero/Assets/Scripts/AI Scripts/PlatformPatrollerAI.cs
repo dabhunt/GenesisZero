e4f1275e1a2b556b72ecda1ecf02c31dded22b8e
Justin Couch <couch.justin.95@gmail.com>
2020-01-27 16:14:18 -0800

    Changes to platform patroller

--- Genesis Zero/Assets/Scripts/AI Scripts/PlatformPatrollerAI.cs
+++ Genesis Zero/Assets/Scripts/AI Scripts/PlatformPatrollerAI.cs
@@ -8,46 +8,46 @@ using UnityEngine;
  */
 public class PlatformPatrollerAI : AIController
 {
-    private Vector3 lookDir = Vector3.up;
-    public float RotationRate = 10f; // How fast to rotate
     public float MoveSpeed = 10f; // Maximum movement speed
     private float targetSpeed = 0.0f;
     public float Acceleration = 5.0f; // Rate of acceleration
-    public float SideDecel = 1.0f; // Rate of deceleration for sideways velocity to create tighter movement
     private Vector3 velocity = Vector3.zero;
     public float AvoidAmount = 1.0f; // How much to accelerate away from the target
     public float AvoidAccelLimit = 1.0f; // Limit on avoidance acceleration
 
+    private Collider mainCollider; // Physical collider for movement
+    public LayerMask GroundMask; // Layermask for ground objects
+
     public float PatrolSpeed = 5.0f; // Movement speed while patrolling
-    public float PatrolRotateRate = 1.0f; // Rotation rate while patrolling
-    private int patrolDir = 1;
+    private int faceDir = 1; // Direction the enemy is facing: 1 = right, -1 = left
 
     public ParticleSystem chargeParticles;
     public ParticleSystem attackParticles;
 
+    protected void Awake()
+    {
+        mainCollider = GetComponent<Collider>();
+    }
+
     protected void Start()
     {
         base.Start();
-        patrolDir = Mathf.RoundToInt(Mathf.Sign(Random.value - 0.5f));
+        faceDir = Mathf.RoundToInt(Mathf.Sign(Random.value - 0.5f));
     }
 
     protected void Update()
     {
         base.Update();
 
-        if (Target == null) { return; }
+        if (Target == null || mainCollider == null) { return; }
 
         if (state == AIState.Follow || state == AIState.Charge || state == AIState.Attack || state == AIState.Cooldown)
         {
-            // Rotation assumes that local up direction is forward
-            lookDir = Vector3.Slerp(lookDir, Target.position - transform.position, RotationRate * Time.deltaTime); // Rotate to face target
             targetSpeed = MoveSpeed;
             Accelerate((transform.position - Target.position).normalized * Mathf.Min(GetAvoidCloseness(), AvoidAccelLimit) * Acceleration * AvoidAmount); // Acceleration to keep away from the target
         }
         else if (state == AIState.Patrol)
         {
-            //For now, patrolling just moves the drone in a circle
-            lookDir = Quaternion.AngleAxis(PatrolRotateRate * patrolDir * Time.deltaTime, Vector3.forward) * lookDir;
             targetSpeed = PatrolSpeed;
         }
         else if (state == AIState.Idle)
@@ -56,8 +56,6 @@ public class PlatformPatrollerAI : AIController
         }
 
         Accelerate(transform.up * (targetSpeed - velocity.magnitude * Mathf.Clamp01(Vector3.Dot(transform.up, velocity.normalized))) * Acceleration); // Accelerate toward the target
-        Accelerate(-transform.right * velocity.magnitude * Vector3.Dot(transform.right, velocity.normalized) * SideDecel); // Deceleration to prevent sideways movement
-        transform.rotation = Quaternion.LookRotation(Vector3.forward, lookDir); // Actual rotation
         transform.Translate(velocity * Time.deltaTime, Space.World); // Actual translation based on velocity
 
         // Particles to show charge and attack states (for testing)
@@ -93,7 +91,7 @@ public class PlatformPatrollerAI : AIController
     }
 
     /**
-     * Accelerates the drone in the given direction
+     * Accelerates the enemy in the given direction
      */
     public void Accelerate(Vector3 accel)
     {
