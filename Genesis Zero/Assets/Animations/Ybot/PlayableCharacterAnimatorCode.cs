9b3652adc6c8eb319d2cedfe2e00128943692df2
AzureFoxStudios <Willporterdesign@outlook.com>
2020-02-01 00:24:27 -0800

    URP_Drone_Character

---
+++ Genesis Zero/Assets/Animations/Ybot/PlayableCharacterAnimatorCode.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayableCharacterAnimatorCode : MonoBehaviour
+{
+    static Animator anim;
+    bool IsFacingRight;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        IsFacingRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+           anim.SetTrigger("isJumping");
+        }
+
+        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+           IsFacingRight = false;
+           gameObject.transform.rotation = Quaternion.EulerRotation(new Vector3(0, -90, 0));
+        }else
+        {
+           IsFacingRight = true;
+           gameObject.transform.rotation = Quaternion.EulerRotation(new Vector3(0, 90, 0));
+        }
+        
+        //How to have a quick "running to stop" code here.
+    }
+}
