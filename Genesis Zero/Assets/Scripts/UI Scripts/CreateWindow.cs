e79fdba77563778440bbd1e5010032c1aee2bcf5
David Hunt <dabhunt@ucsc.edu>
2020-01-31 23:30:49 -0800

    Functional Pause Button UI

---
+++ Genesis Zero/Assets/Scripts/UI Scripts/CreateWindow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateWindow : MonoBehaviour
+{
+    public GameObject windowPrefab;
+    public GameObject buttonPrefab;
+    public GameObject runtimeWindow;
+    public Canvas canvas;  
+
+    void Start()
+    {
+        GameObject tempObj = GameObject.Find("CanvasUI");
+        canvas = tempObj.GetComponent<Canvas>();
+    }
+    void Update()
+    {
+        
+    }
+    public void CreateNewWindow()
+    {
+    	runtimeWindow = Instantiate(windowPrefab);
+        runtimeWindow.transform.SetParent (canvas.transform, false);
+        Time.timeScale = 0f;
+    }
+    public void DestroyWindow()
+    {
+        Time.timeScale = 1.0f;
+        GameObject currentWindow = GameObject.Find("FullscreenWindow(Clone)");
+        DestroyImmediate(currentWindow);
+        print("destroyWindow running..");
+    }
+}
