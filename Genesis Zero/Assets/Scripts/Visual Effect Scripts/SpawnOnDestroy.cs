d83ca496dd3579de7161d0022340eb3cc029d043
Kenny Doan <kennydoan110999@ucsc.edu>
2020-02-05 23:11:19 -0800

    Pickup Update

--- Genesis Zero/Assets/Scripts/Visual Effect Scripts/SpawnOnDestroy.cs
+++ Genesis Zero/Assets/Scripts/Visual Effect Scripts/SpawnOnDestroy.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class SpawnOnDestroy : MonoBehaviour
 {
     public GameObject emitter;
+    public int amount = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnDestroy()
     {
-        GameObject emit = (GameObject)Instantiate(emitter, transform.position, Quaternion.identity);
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject emit = (GameObject)Instantiate(emitter, transform.position, Quaternion.identity);
+        }
+
     }
 }
