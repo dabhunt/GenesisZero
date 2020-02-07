0f06b1b7704ee5c641096c76257726e06526c679
Toan Le <toanle31@gmail.com>
2020-02-06 17:17:07 -0800

    polished movement, and stuff

--- Genesis Zero/Assets/Scripts/Player Scripts/CharacterController.cs
+++ Genesis Zero/Assets/Scripts/Player Scripts/CharacterController.cs
@@ -12,15 +12,16 @@ public class CharacterController : MonoBehaviour
 {
     [Header("Movement")]
     public float acceleration = 20f;
-    public float jumpStrength = 6f;
-    public float doubleJumpStrength = 3f;
+    public float jumpStrength = 13f;
+    public float doubleJumpStrength = 10f;
     public float distToGround = 0.5f; //dist from body origin to ground
     public float bodyRadius = 0.5f; //radius of the spherecast for IsGrounded
+    public float airControlMult = 0.5f;
     public LayerMask ground;
 
     [Header("Physics")]
     public float gravity = 14f;
-    public float terminalVel = 16;
+    public float terminalVel = 16f;
 
     [Header("Camera")]
     public Camera mainCam;
@@ -33,7 +34,8 @@ public class CharacterController : MonoBehaviour
     private PlayerInputActions inputActions;
     private Rigidbody rb;
     private Vector2 movementInput;
-    private Vector2 aimInput;
+    private Vector2 aimInputMouse;
+    private Vector2 aimInputController;
     private Vector3 moveVec = Vector3.zero;
     private Vector3 aimVec = Vector3.zero;
 
@@ -45,7 +47,8 @@ public class CharacterController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         inputActions = new PlayerInputActions();
         inputActions.PlayerControls.Move.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
-        inputActions.PlayerControls.LookAim.performed += ctx => aimInput = ctx.ReadValue<Vector2>();
+        inputActions.PlayerControls.AimMouse.performed += ctx => aimInputMouse = ctx.ReadValue<Vector2>();
+        inputActions.PlayerControls.AimController.performed += ctx => aimInputController = ctx.ReadValue<Vector2>();
     }
 
     private void Update()
@@ -89,13 +92,23 @@ public class CharacterController : MonoBehaviour
         }
 
         //Debug.Log(movementInput);
+        float multiplier = IsGrounded() ? 1 : airControlMult;
+        //Debug.Log(multiplier);
         if (movementInput.x != 0)
         {
             var input = movementInput.x < 0 ? Mathf.Floor(movementInput.x) : Mathf.Ceil(movementInput.x);// this is to deal with left stick returning floats
-            currentSpeed += input * acceleration * Time.fixedDeltaTime;
-            currentSpeed = Mathf.Min(Mathf.Abs(currentSpeed), Mathf.Abs(GetComponent<Player>().GetSpeed().GetValue())) * input;
+            currentSpeed += input * acceleration * multiplier * Time.fixedDeltaTime;
+            if (input > 0)
+            {
+                currentSpeed = Mathf.Min(currentSpeed, GetComponent<Player>().GetSpeed().GetValue());
+            }
+            else
+            {
+                currentSpeed = Mathf.Max(currentSpeed, -(GetComponent<Player>().GetSpeed().GetValue()));
+            }
         }
-        else
+
+        if (movementInput.x == 0)
         {
             if (currentSpeed > 0)
             {
@@ -120,6 +133,7 @@ public class CharacterController : MonoBehaviour
     public bool IsGrounded()
     {
         RaycastHit hit;
+        //bool isGrounded = Physics.BoxCast(transform.position, new Vector3(bodyRadius, 0, 0), Vector3.down, out hit, Quaternion.identity, distToGround, ground, QueryTriggerInteraction.UseGlobal);
         bool isGrounded = Physics.SphereCast(transform.position, bodyRadius, Vector3.down, out hit, distToGround, ground, QueryTriggerInteraction.UseGlobal);
         if (isGrounded != false && hit.collider.isTrigger)
             isGrounded = false;
@@ -138,11 +152,15 @@ public class CharacterController : MonoBehaviour
         {
             vertVel = jumpStrength;
         }
-        Debug.Log(canDoubleJump);
+        //Debug.Log(canDoubleJump);
         if (!IsGrounded() && canDoubleJump)
         {
-            vertVel = jumpStrength;
+            vertVel = doubleJumpStrength;
             canDoubleJump = false;
+            if (moveVec.x > 0 && movementInput.x <= 0)
+                moveVec.x = 0;
+            if (moveVec.x < 0 && movementInput.x >= 0)
+                moveVec.x = 0;
         }
     }
     /* This function control character aiming
@@ -152,8 +170,8 @@ public class CharacterController : MonoBehaviour
     private void Aim()
     {
         Vector3 pos = mainCam.WorldToScreenPoint(transform.position);
-        aimVec.x = aimInput.x - pos.x; 
-        aimVec.y = aimInput.y - pos.y;
+        aimVec.x = aimInputMouse.x - pos.x; 
+        aimVec.y = aimInputMouse.y - pos.y;
 
         float tmpAngle = Mathf.Atan2(aimVec.y, aimVec.x) * Mathf.Rad2Deg;
         if (tmpAngle != 0)
