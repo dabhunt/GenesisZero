5431e84efa2fbc31780934e1e131ecb54b6cbc1c
Justin Couch <couch.justin.95@gmail.com>
2020-02-17 15:31:32 -0800

    Created PlatformShooter enemy

--- Genesis Zero/Assets/Scripts/AI Scripts/AIControllers/PlatformShooterAI.cs
+++ Genesis Zero/Assets/Scripts/AI Scripts/AIControllers/PlatformShooterAI.cs
@@ -5,24 +5,22 @@ using UnityEngine;
 [RequireComponent(typeof(FakeRigidbody))]
 /**
  * Justin Couch
- * DroneAI is the class representing the flying drone enemy type.
+ * PlatformShooterAI is the class representing the platform shooter enemy type.
  */
-public class DroneAI : AIController
+public class PlatformShooterAI : AIController
 {
     protected FakeRigidbody frb;
 
-    private Vector3 lookDir = Vector3.up;
-    public float RotationRate = 10f; // How fast to rotate
     public float MoveSpeed = 10f; // Maximum movement speed
     private float targetSpeed = 0.0f;
     public float Acceleration = 5.0f; // Rate of acceleration
-    public float SideDecel = 1.0f; // Rate of deceleration for sideways velocity to create tighter movement
     public float AvoidAmount = 1.0f; // How much to accelerate away from the target
     public float AvoidAccelLimit = 1.0f; // Limit on avoidance acceleration
 
     public float PatrolSpeed = 5.0f; // Movement speed while patrolling
-    public float PatrolRotateRate = 1.0f; // Rotation rate while patrolling
-    private int patrolDir = 1;
+    public float PatrolSwitchRate = 1.0f; // Rate at which the enemy switches directions while patrolling
+    private int faceDir = 1; // Direction the enemy is facing: 1 = right, -1 = left
+    public float MaxFollowHeight = 5.0f; // Maximum height above the enemy for which the target will be tracked after going out of sight
 
     public ParticleSystem chargeParticles;
     public ParticleSystem attackParticles;
@@ -39,7 +37,21 @@ public class DroneAI : AIController
     new protected void Start()
     {
         base.Start();
-        patrolDir = Mathf.RoundToInt(Mathf.Sign(Random.value - 0.5f));
+        faceDir = Mathf.RoundToInt(Mathf.Sign(Random.value - 0.5f));
+    }
+
+    new protected void Update()
+    {
+        base.Update();
+    }
+
+    protected override void SetTarget(Transform tr)
+    {
+        base.SetTarget(tr);
+        if (Target != null && tracker != null)
+        {
+            tracker.GiveUpCondition = () => { return tracker.PeekFirstPoint().y > transform.position.y + MaxFollowHeight; };
+        }
     }
 
     new protected void FixedUpdate()
@@ -49,16 +61,14 @@ public class DroneAI : AIController
 
         if (state == AIState.Follow || state == AIState.Charge || state == AIState.Attack || state == AIState.Cooldown)
         {
-            // Rotation assumes that local up direction is forward
-            lookDir = Vector3.Slerp(lookDir, targetPosition - transform.position, RotationRate * Time.fixedDeltaTime); // Rotate to face target
+            faceDir = Mathf.RoundToInt(Mathf.Sign(targetPosition.x - transform.position.x));
             targetSpeed = MoveSpeed;
-            frb.Accelerate((transform.position - targetPosition).normalized * Mathf.Min(GetAvoidCloseness(), AvoidAccelLimit) * Acceleration * AvoidAmount); // Acceleration to keep away from the target
+            frb.Accelerate(Mathf.Sign(transform.position.x - targetPosition.x) * Vector3.right * Mathf.Min(GetAvoidCloseness(), AvoidAccelLimit) * Acceleration * AvoidAmount); // Acceleration to keep away from the target
         }
         else if (state == AIState.Patrol)
         {
-            //For now, patrolling just moves the drone in a circle
-            lookDir = Quaternion.AngleAxis(PatrolRotateRate * patrolDir * Time.fixedDeltaTime, Vector3.forward) * lookDir;
             targetSpeed = PatrolSpeed;
+            faceDir = Mathf.RoundToInt(Mathf.Sign(Mathf.Sin(Time.time * PatrolSwitchRate)));
         }
         else if (state == AIState.Idle)
         {
@@ -66,9 +76,8 @@ public class DroneAI : AIController
         }
 
         targetSpeed *= GetSpeed().GetValue();
-        frb.Accelerate(transform.up * (targetSpeed - frb.GetVelocity().magnitude * Mathf.Clamp01(Vector3.Dot(transform.up, frb.GetVelocity().normalized))) * Acceleration); // Accelerate toward the target
-        frb.Accelerate(-transform.right * frb.GetVelocity().magnitude * Vector3.Dot(transform.right, frb.GetVelocity().normalized) * SideDecel); // Deceleration to prevent sideways movement
-        transform.rotation = Quaternion.LookRotation(Vector3.forward, lookDir); // Actual rotation
+        frb.Accelerate(Vector3.right * (targetSpeed * faceDir - frb.GetVelocity().x) * Acceleration); // Accelerate toward the target
+        transform.rotation = Quaternion.LookRotation(Vector3.forward * faceDir, Vector3.up);
 
         // Particles to show charge and attack states (for testing)
         if (chargeParticles != null)
@@ -109,7 +118,7 @@ public class DroneAI : AIController
                 attackLaunchTime = AttackLaunchInterval;
                 if (AttackProjectile != null)
                 {
-                    GameObject spawnedProjectile = Instantiate(AttackProjectile, transform.position, transform.rotation);
+                    GameObject spawnedProjectile = Instantiate(AttackProjectile, transform.position, Quaternion.LookRotation(Vector3.forward, Vector3.right * faceDir));
                     Hitbox spawnedHitbox = spawnedProjectile.GetComponent<Hitbox>();
                     if (spawnedHitbox != null)
                     {
