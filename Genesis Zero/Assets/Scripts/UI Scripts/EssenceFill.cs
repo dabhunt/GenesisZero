eb0d8e7fcbcf10265652f827c389497bfa14d25c
David Hunt <dabhunt@ucsc.edu>
2020-02-19 22:22:29 -0800

    Essence Front End script bug free

--- Genesis Zero/Assets/Scripts/UI Scripts/EssenceFill.cs
+++ Genesis Zero/Assets/Scripts/UI Scripts/EssenceFill.cs
@@ -18,8 +18,18 @@ public class EssenceFill : MonoBehaviour
         //player = temp.GetComponent<Player>();
         //float fillvalue = player.GetHealth().GetValue();
 
-       TestEssence = Random.Range(1,25);
-       InvokeRepeating("CalculateEssenceUI", .2f, .3f);
+       TestEssence = 0;
+       //update UI every .3 seconds, starting in .3 seconds;
+       //InvokeRepeating("test", .3f,.3f);
+       InvokeRepeating("CalculateEssenceUI", .5f, .5f);
+       
+    }
+    void test()
+    {
+    	TestEssence ++;
+    	if (TestEssence > 25){
+    		TestEssence = 0;
+    	}
     }
     void CalculateEssenceUI()
     {
@@ -29,18 +39,30 @@ public class EssenceFill : MonoBehaviour
     	//check if the player has more essence than can be stored
     	if (essenceQuantity <= EssencePerCapsule*TotalCapsules){
     	//determine how many capsules can be filled 100% up
-	    	for (int i = 0; i < essenceQuantity; i++)
+	    	for (int i = 1; i <= essenceQuantity; i++)
 	        {
-	            if ((i % TotalCapsules) == 0)
+	            if ((i % EssencePerCapsule) == 0)
 	            {
 	                capsule[capsuleCount].fillAmount = 1.0f;
 	                capsuleCount++;
 	            }
 	        }
-	        //calculate how much essence can be put in the remaining capsule on UI
-	        var remainderFill = essenceQuantity % EssencePerCapsule;
-	        remainderFill = remainderFill/EssencePerCapsule;
-	        capsule[capsuleCount].fillAmount = remainderFill;
+
+	        if (capsuleCount <= TotalCapsules-1){
+		        //calculate how much essence can be put in the remaining capsule on UI
+		        float remainderFill = essenceQuantity % EssencePerCapsule;
+		        //print("remainderFill after just %" remainderFill);
+		        remainderFill = remainderFill/EssencePerCapsule;
+		        //print("remainderFill:" remainderFill);
+		        capsule[capsuleCount].fillAmount = remainderFill;
+		        capsuleCount++;
+		        while (capsuleCount < TotalCapsules)
+		        {
+	        		capsule[capsuleCount].fillAmount = 0f;
+	        		capsuleCount++;
+	        	}
+	        }
+	        //In case subtraction has happened, calculate if we need to remove fill from other capsules
 	    } else
 	    {
 	    	print("error: too much essence for my delicate UI to handle");
