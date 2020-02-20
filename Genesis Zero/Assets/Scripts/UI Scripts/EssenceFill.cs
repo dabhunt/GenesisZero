c7893486b68116eb182e8cc885edc41b1e657c2c
David Hunt <dabhunt@ucsc.edu>
2020-02-19 22:44:45 -0800

    Merged Backend Essence statistic

--- Genesis Zero/Assets/Scripts/UI Scripts/EssenceFill.cs
+++ Genesis Zero/Assets/Scripts/UI Scripts/EssenceFill.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class EssenceFill : MonoBehaviour
 {
 	public Image[] capsule;
-    public int TestEssence;
+    public float TestEssence;
     public int EssencePerCapsule;
     public int TotalCapsules;
     private Player player;
@@ -14,11 +14,9 @@ public class EssenceFill : MonoBehaviour
     void Awake()
     {
         //will use this code once essence is stored on the player
-        //GameObject temp = GameObject.FindWithTag("Player");
-        //player = temp.GetComponent<Player>();
-        //float fillvalue = player.GetHealth().GetValue();
-
-       TestEssence = 0;
+       GameObject temp = GameObject.FindWithTag("Player");
+       player = temp.GetComponent<Player>();
+       TestEssence = player.GetEssenceAmount();
        //update UI every .3 seconds, starting in .3 seconds;
        //InvokeRepeating("test", .3f,.3f);
        InvokeRepeating("CalculateEssenceUI", .5f, .5f);
@@ -34,8 +32,8 @@ public class EssenceFill : MonoBehaviour
     void CalculateEssenceUI()
     {
     	print("calculating essence...");
-    	var essenceQuantity = TestEssence;
-    	var capsuleCount = 0;
+    	float essenceQuantity = player.GetEssenceAmount();
+    	int capsuleCount = 0;
     	//check if the player has more essence than can be stored
     	if (essenceQuantity <= EssencePerCapsule*TotalCapsules){
     	//determine how many capsules can be filled 100% up
