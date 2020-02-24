1509eca3d86a180a1dd6f9f4045b36a07e0f35df
Toan Le <toanle31@gmail.com>
2020-02-23 19:27:14 -0800

    Fix CrossBloom too fast

--- Genesis Zero/Assets/Scripts/UI Scripts/SacrificeUI.cs
+++ Genesis Zero/Assets/Scripts/UI Scripts/SacrificeUI.cs
@@ -18,42 +18,25 @@ public class SacrificeUI : MonoBehaviour
     private void Wake()
     {
         //inputActions.PlayerControls.Move.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
-        inputActions.MenuControls.MoveSelect.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
-        inputActions.MenuControls.Select.performed += ctx => selectInput = ctx.ReadValue<float>();
         inputActions = new PlayerInputActions();
     }
-    void Start()
+    private void Start()
     {
-        GameObject temp = GameObject.FindGameObjectWithTag("Player");
-        player = temp.GetComponent<Player>();
-        playerController = player.GetComponent<PlayerController>();
-        SetPosition(5f);
+        
     }
-    //
     //if player presses on a hardware mod in the bar above, it highlights
-    void changeSelection()
+    private void changeSelection()
     {
-        if (moveInput.x > 0)
-        {
-            selectionObj.transform.position += Vector3.right * (0.765f+75f);
-        }
-        if (moveInput.x < 0)
-        {
-            selectionObj.transform.position -= Vector3.right * (0.765f+75f);   
-        }
+        
     }
-    void SetPosition(float xValue)
+    private void SetPosition(float xValue)
     {
         selectionObj.transform.position = new Vector3(xValue,1,1);
     }
-    void openSacrificeWindow()
+    private void openSacrificeWindow()
     {
         ModUI = GameObject.FindGameObjectsWithTag("ModUI");
         selectedMod = ModUI[1];
         SkillObject skill = selectedMod.GetComponent<SkillUIElement>().GetComponent<SkillObject>();
-    }
-    void ScrapMod()
-    {
-        player.GetSkillManager().RemoveSkill(skill);
     }
 }
