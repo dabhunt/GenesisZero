1c271084b9e6983946f9b1aaffba1896ebcaf3f4
David Hunt <dabhunt@ucsc.edu>
2020-02-28 15:08:48 -0800

    Huge Tileset visual update walkable surfaces

--- Genesis Zero/Assets/Scripts/Player Scripts/Skill Scripts/SkillManager.cs
+++ Genesis Zero/Assets/Scripts/Player Scripts/Skill Scripts/SkillManager.cs
@@ -114,7 +114,11 @@ public class SkillManager
     {
         Object[] skills = Resources.LoadAll("Skills");
         SkillObject skill = (SkillObject)skills[Random.Range(0, skills.Length)];
-        //Debug.Log(skill.name);
+        //this fixes the problem of this function also returning abilities
+        while (skill.IsAbility)
+        {
+            skill = (SkillObject)skills[Random.Range(0, skills.Length)];
+        }
         return skill;
     }
 
