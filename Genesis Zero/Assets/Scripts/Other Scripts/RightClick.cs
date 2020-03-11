201e8bda839ecb3044a7f9c3117c201ab0b3108c
David Hunt <dabhunt@ucsc.edu>
2020-03-11 02:02:54 -0700

    Right click to drop Modifier
    currently it puts it on the ground, but this opens up the possibility of people dropping all modifiers they like when they encounter a god head. I think either a 50% chance of it dropping on the ground, or it's just discarding would prevent this behavior.

---
+++ Genesis Zero/Assets/Scripts/Other Scripts/RightClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class RightClick : MonoBehaviour
+{
+    private Player player;
+    private SkillManager sManager;
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+        sManager = player.GetSkillManager();
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            Debug.Log("removing stacks...");
+            RemoveStack();
+        }
+
+    }
+    public void RemoveStack()
+    {
+        SkillObject s = GetComponent<SkillUIElement>().Skill;
+        sManager.RemoveSkill(s);
+        sManager.SpawnMod(player.transform.position, s.name);
+    }
+}
