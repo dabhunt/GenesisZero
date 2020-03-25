b48c18bafb6a88e7ae31497713858451c93fe1f5
Dot <Willporterdesign@outlook.com>
2020-03-24 22:59:56 -0700

    Gun Has Shader, Heat is now visible

---
+++ Genesis Zero/Assets/Scripts/Visual Effect Scripts/ShaderGraphScripts/GunMaterial.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunMaterial: MonoBehaviour
+{
+    OverHeat overheat;
+    void Start()
+    {
+        GameObject temp = GameObject.FindWithTag("Player");
+        overheat = temp.GetComponent<Player>().GetComponent<OverHeat>();
+        gameObject.GetComponent<Renderer>().material.SetFloat("_HeatControl", 0);
+    }
+    void Update()
+    {
+        float ratio = overheat.GetHeat() / overheat.GetMaxHeat();
+        gameObject.GetComponent<Renderer>().material.SetFloat("_HeatControl", ratio);
+    }
+}
