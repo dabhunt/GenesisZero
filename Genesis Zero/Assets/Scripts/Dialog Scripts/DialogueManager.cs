a796cc3ba545dd358b91c3007a5f2876a0d3bba4
David Hunt <dabhunt@ucsc.edu>
2020-04-01 13:37:54 -0700

    Deleted Sus Files

--- Genesis Zero/Assets/Scripts/Dialog Scripts/DialogueManager.cs
+++ Genesis Zero/Assets/Scripts/Dialog Scripts/DialogueManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 using System.IO;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -11,11 +12,14 @@ public class DialogueManager : MonoBehaviour
     public Dialogue dialogue;
 
     //public Text nameText;
-    public Text dialogueText;
-    public GameObject canv;
-
+    public TextMeshProUGUI dialogueText;
+    public Image  charImage;
+    public GameObject parent;
+    private Sprite[] iconArray;
+    private Queue<string> charIcons;
     private Queue<string> sentences;
     private Queue<string> durations;
+
 
     void Awake()
     {
@@ -29,22 +33,25 @@ public class DialogueManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
 
     }
-
     // Start is called before the first frame update
     void Start()
     {
+        charIcons = new Queue<string>();
         sentences = new Queue<string>();
         durations = new Queue<string>();
+        instance.EndDialogue();
+        instance.TriggerDialogue("TestDialog.txt");
     }
 
     public void TriggerDialogue(string filePath)
     {
         /*
          * read each line and store each entity separated by a ';' or '\n'
-         * first label = text
-         * second label = duration of text
-         * third label = voice clip if available
-         * assumptions: first two labels always exist, third label may not exist,
+         * first label = character saying the text
+         * second label = text
+         * third label = duration of text (-1 means it will wait until player input to get to the next sentence)
+         * (optional) fourth label = voice clip if available
+         * assumptions: first 3 labels always exist, fourth label may not exist,
          * */
 
         string path = "Assets/Resources/Dialogue/";
@@ -53,6 +60,7 @@ public class DialogueManager : MonoBehaviour
 
         int count = 0;
         string line;
+        dialogue.charIcons = new Queue<string>();
         dialogue.sentences = new Queue<string>();
         dialogue.durations = new Queue<string>();
 
@@ -78,14 +86,14 @@ public class DialogueManager : MonoBehaviour
         while ((line = reader.ReadLine()) != null)
         {
             string[] output = line.Split(';');
-            dialogue.sentences.Enqueue(output[0]);
-            dialogue.durations.Enqueue(output[1]);
+            dialogue.charIcons.Enqueue(output[0]);
+            dialogue.sentences.Enqueue(output[1]);
+            dialogue.durations.Enqueue(output[2]);
             count++;
-            if (output.Length > 2)
-                AudioManager.instance.PlaySound(output[2]);
+            if (output.Length > 3)
+                AudioManager.instance.PlaySound(output[3]);
                 //Debug.Log(output[2]);
         }
-
         reader.Close();
         instance.StartDialogue(dialogue);
     }
@@ -93,11 +101,15 @@ public class DialogueManager : MonoBehaviour
     public void StartDialogue (Dialogue dialogue)
     {
         //nameText.text = dialogue.name;
-
+        parent.SetActive(true);
+        charIcons.Clear();
         sentences.Clear();
         durations.Clear();
-
-        while(dialogue.sentences.Count > 0) 
+        while (dialogue.charIcons.Count > 0)
+        {
+            charIcons.Enqueue(dialogue.charIcons.Dequeue());
+        }
+        while (dialogue.sentences.Count > 0) 
         {
             sentences.Enqueue(dialogue.sentences.Dequeue());
         }
@@ -117,17 +129,25 @@ public class DialogueManager : MonoBehaviour
             EndDialogue();
             return;
         }
-
+        string charIcon = charIcons.Dequeue();
         string sentence = sentences.Dequeue();
         string duration = durations.Dequeue();
         StopAllCoroutines();
-        StartCoroutine(TypeSentence(sentence, duration));
-        
+        StartCoroutine(TypeSentence(charIcon, sentence, duration));
     }
 
-    IEnumerator TypeSentence(string sentence, string dur)
+    IEnumerator TypeSentence(string charIcon, string sentence, string dur)
     {
         dialogueText.text = "";
+        //names in this folder HAVE to match the .txt file names of characters in order to load
+        charIcon.ToUpper();
+        Sprite spr = Resources.Load<Sprite>("Dialogue/CharacterIcons/" + charIcon);
+        //Sprite spr = null;
+        //print("spr.name: " + spr.name);
+        if (spr != null)
+        {
+            charImage.sprite = spr;
+        }
         foreach (char letter in sentence.ToCharArray())
         {
             dialogueText.text += letter;
@@ -142,11 +162,14 @@ public class DialogueManager : MonoBehaviour
 
     void EndDialogue()
     {
-        canv.SetActive(false);
+        parent.SetActive(false);
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            DisplayNextSentence();
+        }
     }
 }
