fc65d89256a0ae2dae58bd583bad8502ac107849
Toan Le <toanle31@gmail.com>
2020-04-05 16:34:03 -0700

    Transition from menu scene to level with progress bar

---
+++ Genesis Zero/Assets/Scripts/Other Scripts/LevelLoader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public static LevelLoader instance;
+    public GameObject loadingScreen;
+    public Slider loadBar;
+    public Text loadPercentage;
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    public void LoadFromMenu(string sceneName)
+    {
+        StartCoroutine(LoadFromMenuCoRoutine(sceneName));
+    }
+
+    IEnumerator LoadFromMenuCoRoutine (string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        loadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            loadBar.value = progress;
+            loadPercentage.text = progress * 100 + " %";
+            Debug.Log(progress);
+            yield return null;
+        }
+    }
+}
