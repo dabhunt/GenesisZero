619c1e37be2cd46ddf45681a4f87f8f708e37d0e
David Hunt <dabhunt@ucsc.edu>
2020-04-11 18:00:18 -0700

    Shield Boy knockback / fixed shield collider + menu scene change

--- Genesis Zero/Assets/Scripts/AI Scripts/AIControllers/ShieldEnemyAI.cs
+++ Genesis Zero/Assets/Scripts/AI Scripts/AIControllers/ShieldEnemyAI.cs
@@ -212,6 +212,8 @@ public class ShieldEnemyAI : AIController
             Vector3 lungeDir = (normalizedTargetDir + Vector3.up * Mathf.Abs(Vector3.Dot(normalizedTargetDir, Vector3.right)) * LungeVerticality).normalized;
             //}
             frb.AddVelocity(lungeDir * LungeSpeed);
+            print("jumptoward runs");
+            SpawnAttackHitbox();
         }
     }
 
@@ -226,6 +228,8 @@ public class ShieldEnemyAI : AIController
             spawnedHitboxObj.localPosition = AttackHitboxStart;
             spawnedHitboxObj.localRotation = Quaternion.identity;
             Hitbox spawnedHitbox = spawnedHitboxObj.GetComponent<Hitbox>();
+            spawnedHitbox.DirectionalKnockback = true;
+            spawnedHitbox.Knockbackforce = 20f;
             spawnedHitbox.InitializeHitbox(GetDamage().GetValue(), this);
         }
     }
