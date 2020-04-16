702497dcf29e738b2d7ea02273939b66fcbb08c0
David Hunt <dabhunt@ucsc.edu>
2020-04-15 17:48:04 -0700

    Recursive Material Replacement Script + Kyle Background buildings in main scene

---
+++ Genesis Zero/Assets/Scripts/Other Scripts/BackgroundRecursive.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundRecursive : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Material material;
+    void Start()
+    {
+        if (material != null)
+            ReplaceMaterialRecursive(this.transform, material);
+    }
+    public void ReplaceMaterialRecursive(Transform t, Material mat)
+    {
+            foreach (Transform child in t)
+            {
+                if (child.gameObject.GetComponent<MeshRenderer>() != null)
+                {
+                    child.gameObject.GetComponent<MeshRenderer>().material = mat;
+                }
+                Renderer r = child.gameObject.GetComponent<Renderer>();
+                if (child.childCount > 0)
+                {
+                    ReplaceMaterialRecursive(child, mat);
+                }
+            }
+        }
+}
