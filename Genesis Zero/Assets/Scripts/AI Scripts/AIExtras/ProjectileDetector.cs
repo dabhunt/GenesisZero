fc378bccd0271f9c559aacb3c7118867514c2704
Justin Couch <couch.justin.95@gmail.com>
2020-04-17 13:08:42 -0700

    Added ProjectileDetector script

---
+++ Genesis Zero/Assets/Scripts/AI Scripts/AIExtras/ProjectileDetector.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Justin Couch
+ * ProjectileDetector is used for detecting collisions with projectiles without destroying them
+ */
+public class ProjectileDetector : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+    }
+}
