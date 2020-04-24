e80ef14012c8a371a92abb647eb6027ea9922784
Dot <Willporterdesign@outlook.com>
2020-04-24 02:41:41 -0700

    Portal Changes Pt.1 (Not ready to pull)

--- Genesis Zero/Assets/Scripts/PCG Scripts/Teleporter.cs
+++ Genesis Zero/Assets/Scripts/PCG Scripts/Teleporter.cs
@@ -2,31 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Teleporter : MonoBehaviour
 {
 	public float destinationX = -120;
 	public float destinationY = 16;
 	public float destinationZ = 0;
-	private void Start() 
+
+	//Variables for animator
+	public Transform player;
+	public float portalactivedistance=1f;
+	Animator ani;
+
+	private void Start()
 	{
+		player = GameObject.FindWithTag("Player").transform;	//Grabs the player transform position to use in the rest of the script.
+		ani = GetComponent<Animator>();
+
 	}
     private void OnTriggerEnter(Collider other)
 	{
 		if (other.GetComponent<Player>())
     	{
 			//thePlayer.transform.position = destination.position;
+
 			//thePlayer.transform.position = new Vector3(-120, 16, 0);
+			if(GetAnimationStateTime()>=1){
 			teleport();
 		}
-		
+		}
+
 	}
 	private void Update()
 	{
-
+		portalAnimation();
 	}
 	private void teleport()
 	{
-		GameObject.FindWithTag("Player").transform.position = new Vector2(destinationX, destinationY);
+		player.position = new Vector2(destinationX, destinationY);
 		TileManager.instance.curlevel++;
 	}
 	public void SetDesination(Vector2 destination)
@@ -35,5 +48,31 @@ public class Teleporter : MonoBehaviour
 		destinationY = destination.y;
 		destinationZ = 0;
 	}
+
+	public float GetAnimationStateTime(){
+		return Mathf.Repeat(ani.GetCurrentAnimatorStateInfo(0).normalizedTime, 1f)+.1f;
+	}
+
+	public void portalAnimation()
+    {
+			if(distanceFromPortal()<portalactivedistance){
+				ani.SetBool("Open",true);
+				ani.SetBool("Close",false);
+			}
+			if(distanceFromPortal()>portalactivedistance){
+				ani.SetBool("Close",true);
+				ani.SetBool("Open",false);
+			}
+
+
+    }
+
+	public float distanceFromPortal()
+    {
+		float distance;
+
+
+		distance=Vector3.Distance(transform.position,player.position); //keeps everything a positive value and returns the distance
+		return distance;
+    }
 }
- 
