7e42558a7c2d19f42ceebcd73de59543d97b83ac
Kyle C. Fegan <32467192+kylefegan@users.noreply.github.com>
2020-04-29 12:55:10 -0700

    Clean up TileManager

--- Genesis Zero/Assets/Scripts/PCG Scripts/TileManager.cs
+++ Genesis Zero/Assets/Scripts/PCG Scripts/TileManager.cs
@@ -53,7 +53,8 @@ public class TileManager : MonoBehaviour
 	}
 	private void Start()
     {
-		tilePrefabs = industrialTilePrefabs;
+		//tilePrefabs = industrialTilePrefabs;
+		tilePrefabs = cityTilePrefabs;
 		
 		//Level 1
 		int level = 0;
