25336e434a8e134777e97aeaa281a1519aedb2d1
Toan Le <toanle31@gmail.com>
2020-04-30 17:27:12 -0700

    Pause Menu now has options for sound and resolution

--- Genesis Zero/Assets/Scripts/Other Scripts/StartMenu.cs
+++ Genesis Zero/Assets/Scripts/Other Scripts/StartMenu.cs
@@ -11,43 +11,15 @@ public class StartMenu : MonoBehaviour
     private AsyncOperation operation;
     private Slider loadBar;
     private Text loadPercentage;
-
+    private GameObject loadingScreen;
     private GameObject mainMenuScreen;
     private GameObject optionsScreen;
-    private GameObject loadingScreen;
-    private TMPro.TMP_Dropdown resDropdown;
-    private List<Resolution> resolutions;
-
+    
     public void Start()
     {
-        List<string> values;
-        //Caching some canvas groups
-        mainMenuScreen = canvas.transform.Find("MainMenuScreen").gameObject;
         loadingScreen = canvas.transform.Find("LoadingScreen").gameObject;
+        mainMenuScreen = canvas.transform.Find("MainMenuScreen").gameObject;
         optionsScreen = canvas.transform.Find("OptionsScreen").gameObject;
-        resDropdown = optionsScreen.transform.Find("Resolution").GetComponent<TMPro.TMP_Dropdown>();
-
-        //Populating Resolution list
-        Resolution[] options = Screen.resolutions;
-        resolutions = new List<Resolution>(options);
-        values = new List<string>();
-        resolutions.Reverse();
-
-        int index = 0;
-        for (int i = 0; i < resolutions.Count; i++)
-        {
-            if (resolutions[i].refreshRate < 60)
-                continue;
-            values.Add(resolutions[i].width + "x" + resolutions[i].height + " (" + resolutions[i].refreshRate + " hz)");
-            if (resolutions[i].height == Screen.currentResolution.height && resolutions[i].width == Screen.currentResolution.width)
-            {
-                index = i;
-            }
-        }
-        resDropdown.ClearOptions();
-        resDropdown.AddOptions(values);
-        resDropdown.value = index;
-        resDropdown.RefreshShownValue();
     }
 
     //Onclick Event for Start Button
@@ -84,100 +56,6 @@ public class StartMenu : MonoBehaviour
         optionsScreen.SetActive(false);
         mainMenuScreen.transform.Find("Buttons").gameObject.SetActive(true);
         mainMenuScreen.SetActive(true);
-    }
-
-    //Event to set master volume
-    public void SetMasterVolume(float value)
-    {
-        if (value >= -40f)
-            AudioManager.instance.mixer.SetFloat("masterVolume", value);
-        else
-            AudioManager.instance.mixer.SetFloat("masterVolume", -80f);
-    }
-
-    //Event to set sfx volume
-    public void SetSFXVolume(float value)
-    {
-        if (value >= -40f)
-            AudioManager.instance.mixer.SetFloat("sfxVolume", value);
-        else
-            AudioManager.instance.mixer.SetFloat("sfxVolume", -80f);
-    }
-
-    //Event to set music volume
-    public void SetMusicVolume(float value)
-    {
-        if (value >= -40f)
-            AudioManager.instance.mixer.SetFloat("musicVolume", value);
-        else
-            AudioManager.instance.mixer.SetFloat("musicVolume", -80f);
-    }
-
-    //Event for toggle mute(master)
-    public void MuteMaster(bool value)
-    {
-        if (value)
-        {
-            SetInteractable("Master", false);
-            SetMasterVolume(-80f);
-        }
-        else
-        {
-            SetInteractable("Master", true);
-            SetMasterVolume(GetSliderValue("Master"));
-        }
-    }
-
-    //Event for toggle mute(sfx)
-    public void MuteSFX(bool value)
-    {
-        if (value)
-        {
-            SetInteractable("SFX", false);
-            SetSFXVolume(-80f);
-        }
-        else
-        {
-            SetInteractable("SFX", true);
-            SetSFXVolume(GetSliderValue("SFX"));
-        }
-    }
-
-    //Event for toggle mute(music)
-    public void MuteMusic(bool value)
-    {
-        if (value)
-        {
-            SetInteractable("Music", false);
-            SetMusicVolume(-80f);
-        }
-        else
-        {
-            SetInteractable("Music", true);
-            SetMusicVolume(GetSliderValue("Music"));
-        }
-    }
-
-    private float GetSliderValue(string name)
-    {
-        float volume = optionsScreen.transform.Find("AudioSettings").Find(name).gameObject.GetComponent<Slider>().value;
-        return volume;
-    }
-
-    private void SetInteractable(string name, bool value)
-    {
-        Slider slider = optionsScreen.transform.Find("AudioSettings").Find(name).gameObject.GetComponent<Slider>();
-        slider.interactable = value;
-    }
-
-    public void SetFullScreen(bool value)
-    {
-        Screen.fullScreen = value;
-    }
-    //Event to set Resolution
-    public void SetResolution(int index)
-    {
-        Screen.SetResolution(resolutions[index].width, resolutions[index].height, Screen.fullScreen, resolutions[index].refreshRate);
     }
 
     private void LoadScene()
--- Genesis Zero/Assets/Scripts/Other Scripts/StateManager.cs
+++ Genesis Zero/Assets/Scripts/Other Scripts/StateManager.cs
@@ -16,6 +16,8 @@ public class StateManager : MonoBehaviour
     public GameObject canvas;
     public GameObject pauseMenu;
     private AsyncOperation operation;
+    private GameObject optionsMenu;
+    private GameObject pMenuButtons;
     private void Awake()
     {
         if (instance == null)
@@ -34,6 +36,7 @@ public class StateManager : MonoBehaviour
         restart = temp.GetComponent<Restart>();
         canvas = GameObject.FindWithTag("CanvasUI");
         pauseMenu = canvas.transform.Find("PauseMenu").gameObject;
+        optionsMenu = canvas.transform.Find("OptionsScreen").gameObject;
     }
     private void Update()
     {
@@ -151,6 +154,12 @@ public class StateManager : MonoBehaviour
     {
         StartCoroutine(LoadSceneCoroutine());
         //SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public void ToggleOptionsMenu(bool toggle)
+    {
+        pauseMenu.SetActive(!toggle);
+        optionsMenu.SetActive(toggle);
     }
 
     IEnumerator LoadSceneCoroutine()
