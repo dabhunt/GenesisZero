de4ffb1878b9ec26ab991e7c500a06fadc5711de
Jordan Lee <50028638+SpicyMeatloaf@users.noreply.github.com>
2020-05-20 00:04:32 -0700

    Creating credits

---
+++ Genesis Zero/Assets/Scripts/Animation Scripts/EndCredits.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using TMPro;
+
+public class EndCredits : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Color DefaultTextColor = Color.white;
+    public Color DefaultHighlightTextColor = Color.red;
+    public float DefaultShowDuration = 4f;
+    public float DefaultFadeDuration = .7f;
+    public float DefaultTextInactiveDuration = .7f;
+
+    private Queue<CreditCard> cardQueue;
+    private Color overlay;
+    private float fadeDuration;
+    private TextMeshProUGUI tmp;
+    private Image image;
+
+    private void Start()
+    {
+        cardQueue = new Queue<CreditCard>();
+        CreditCard[] objs = Resources.LoadAll<CreditCard>("CreditCards");
+        for (int i = 0; i < objs.Length; i++)
+        {
+            cardQueue.Enqueue(objs[i]);
+        }
+        tmp = transform.Find("Text").gameObject.GetComponent<TextMeshProUGUI>();
+        transform.Find("Overlay").GetComponent<Image>().enabled = true;
+        NextCard();
+        if(GameInputManager.instance != null)
+            GameInputManager.instance.DisablePlayerControls();
+    }
+    public void NextCard()
+    {
+        if (cardQueue.Count < 1)
+        {
+            EndIntro();
+            return;
+        }
+        else
+        {
+            tmp.text = cardQueue.Peek().Name;
+            tmp.color = cardQueue.Peek().MainTextColor;
+            fadeDuration = cardQueue.Peek().FadeDuration;
+            float inactiveDuration = cardQueue.Peek().TextInactiveDuration;
+            float durationOnCard = cardQueue.Peek().ShowDuration;
+            if (fadeDuration == 0)
+                fadeDuration = DefaultFadeDuration;
+            if (inactiveDuration == 0)
+                inactiveDuration = DefaultTextInactiveDuration;
+            if (durationOnCard == 0)
+                durationOnCard = DefaultShowDuration;
+            cardQueue.Dequeue();
+            FadeIn();
+            Invoke("FadeOut", fadeDuration + durationOnCard);
+            Invoke("NextCard", fadeDuration + durationOnCard + fadeDuration + inactiveDuration);
+
+        }
+    }
+    void Update()
+    {
+        transform.Find("Overlay").gameObject.GetComponent<Image>().color = overlay;
+        if (Input.GetKeyDown(KeyCode.Escape))
+            EndIntro();
+    }
+    //public string SplitText()
+    //fade in refers to the text becoming visible, even though the overlay is technically fading out
+    public void FadeIn()
+    {
+        DOTween.To(() => overlay.a, x => overlay.a = x, 0, fadeDuration);
+    }
+    //fade out refers to the text becoming covered by the overlay
+    public void FadeOut()
+    {
+        DOTween.To(() => overlay.a, x => overlay.a = x, 1, fadeDuration);
+    }
+    public void EndIntro()
+    {
+        CancelInvoke();
+        cardQueue.Clear();
+        gameObject.SetActive(false);
+        //gameObject.transform.parent.Find("BlackOverlay").GetComponent<SpriteFade>().FadeOut(4f);
+
+    }
+}
---
+++ Genesis Zero/Assets/Scripts/Other Scripts/CreditCard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[CreateAssetMenu(menuName = "Credit Card")]
+public class CreditCard : ScriptableObject
+{
+    [TextArea]
+    public string Role;
+    public string Name;
+    public Image Image;
+
+    [Tooltip("Words to highlight in different color, Comma seperated list")]
+    public string WordsToHighlight;
+    [Space]
+    //public variables below here have default values if they are set to 0 in inspector
+    [Header("Optional Variables to override defaults")]
+    public Color MainTextColor = Color.white;
+    public Color HighlightTextColor;
+    public float ShowDuration;
+    public float FadeDuration;
+    public float TextInactiveDuration;
+}
