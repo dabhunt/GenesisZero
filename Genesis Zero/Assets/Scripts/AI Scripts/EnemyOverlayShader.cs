899bdf7a2ad0c7bb8166ceebbc726d5dd5a986e9
AzureFoxStudios <Willporterdesign@outlook.com>
2020-05-21 05:21:33 -0700

    NotPullSafe-EssenceUpdate/MuzzleMesh/EnemyShaderDone/CrawleyandShieldTextured/PipeUpdate/Water

--- Genesis Zero/Assets/Scripts/AI Scripts/EnemyOverlayShader.cs
+++ Genesis Zero/Assets/Scripts/AI Scripts/EnemyOverlayShader.cs
@@ -21,7 +21,7 @@ public class EnemyOverlayShader : MonoBehaviour
     void Start()
     {
         gameObject.GetComponent<Renderer>().material.SetFloat("_OnOff", 0);
-        gameObject.GetComponent<Renderer>().material.SetFloat("_elecOnOff", 0);
+        gameObject.GetComponent<Renderer>().material.SetFloat("_StunOnOff", 0);
         gameObject.GetComponent<Renderer>().material.SetFloat("_LowHP", 0);
         gameObject.GetComponent<Renderer>().material.SetFloat("_DissolveEffect", 0);
         gameObject.GetComponent<Renderer>().material.SetFloat("_TimeEffect", 0);
@@ -35,6 +35,9 @@ public class EnemyOverlayShader : MonoBehaviour
         if (isStunned == true)
         {
             gameObject.GetComponent<Renderer>().material.SetFloat("_StunOnOff", 1);
+        } else
+        {
+            gameObject.GetComponent<Renderer>().material.SetFloat("_StunOnOff", 0);
         }
 
         if (hpcurValue < hpvalueLastFrame && hpcurValue > 0)
