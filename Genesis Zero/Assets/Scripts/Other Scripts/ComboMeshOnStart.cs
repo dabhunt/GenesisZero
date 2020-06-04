0e93914954c29ec32b5dfc7142c90388277d6f47
David Hunt <dabhunt@ucsc.edu>
2020-06-04 03:57:30 -0700

    More Roof changes and combo mesh script placements

--- Genesis Zero/Assets/Scripts/Other Scripts/ComboMeshOnStart.cs
+++ Genesis Zero/Assets/Scripts/Other Scripts/ComboMeshOnStart.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class ComboMeshOnStart : MonoBehaviour
 {
     // Start is called before the first frame update
+    public bool MultiMaterial = true;
     void Start()
     {
         if (GetComponent<MeshCombiner>() == null)
         {
             MeshCombiner combiner = gameObject.AddComponent<MeshCombiner>();
-            combiner.CreateMultiMaterialMesh = true;
+            combiner.CreateMultiMaterialMesh = MultiMaterial;
             combiner.DeactivateCombinedChildrenMeshRenderers = true;
             combiner.CombineMeshes(true);
             gameObject.GetComponent<MeshRenderer>().receiveShadows = false;
